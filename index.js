d9a0af9f044e5a89b1efb8fca1ea553b338f7f76
Rebeca Moreno Rodriguez <rebecaxmoreno@gmail.com>
2025-11-20 17:20:32 -0800

    Revert "Implement Audacity-style side menu UI"

--- index.js
+++ index.js
@@ -122,10 +122,6 @@ function attachRemoveListener(soundModule) {
   removeBtn.addEventListener("click", () => {
     const moduleId = parseInt(removeBtn.dataset.moduleId);
 
-    // Ask for confirmation before removing
-    const confirmRemoval = confirm("Are you sure you want to remove this sound module?"); 
-    if (!confirmRemoval) return;
-
     // Remove the corresponding synth and gain node
     if (synths[moduleId]) {
       synths[moduleId].dispose();
@@ -196,13 +192,6 @@ function attachCollapseListener(soundModule) {
     const isVisible = options.style.display === "block";
     options.style.display = isVisible ? "none" : "block";
     collapseBtn.textContent = isVisible ? "▼" : "▲";
-
-    setTimeout(() => {
-      const plotDiv = soundModule.querySelector('.plot');
-      if (plotDiv && plotDiv.data) {
-        Plotly.Plots.resize(plotDiv);
-      }
-    }, 100);
   });
 }
 
@@ -1062,21 +1051,10 @@ function plot(moduleIdx) {
           showgrid: true,
         },
         autosize: true,
-        margin: { l: 100, r: 50, t: 100, b: 100 } // Extra bottom margin for rotated labels
-      };
-
-      // Add config parameter
-      let config = {
-        responsive: true 
       };
 
       // Plot the data using Plotly
-      Plotly.newPlot(m.querySelector(".plot"), plotData, layout, config);
-
-      // Force resize after plot creation
-      setTimeout(() => {
-        Plotly.Plots.resize(m.querySelector(".plot"));
-      }, 100);
+      Plotly.newPlot(m.querySelector(".plot"), plotData, layout);
     }
   }
 }
--- soundModule.js
+++ soundModule.js
@@ -1,116 +1,105 @@
 function createSoundModuleTemplate(moduleId) {
     return `
         <div class="soundModule" id="module${moduleId}">
-            <!-- LEFT SIDE: All menu options -->
-            <div class="leftMenu">
-                <!-- X button at top corner -->
+            <div class="moduleTopOptions">
+                <div class="volumeContainer">
+                    <label for="volume">Volume:</label>
+                    <input type="range" class="volume" min="-60" max="3" value="-10">
+                </div>
                 <button class="removeModule" data-module-id="${moduleId}"></button>
-                
-                <!-- Top options: Volume, Sensor, Reading -->
-                <div class="moduleTopOptions">
-                    <div class="volumeContainer">
-                        <label for="volume">Volume:</label>
-                        <input type="range" class="volume" min="-60" max="3" value="-10">
-                    </div>
+            </div>
+
+            <div class="moduleDataOptions">
+                <div class="sensorsContainer">
+                    <label for="sensors">Sensor:</label>
+                    <select class="sensors">
+                    </select>
                 </div>
 
-                <div class="moduleDataOptions">
-                    <div class="sensorsContainer">
-                        <label for="sensors">Sensor:</label>
-                        <select class="sensors">
-                        </select>
-                    </div>
-
-                    <div class="readingsContainer">
-                        <label for="readings">Reading:</label>
-                        <select class="readings">
-                        </select>
-                    </div>
-                </div>
-                
-                <!-- Collapse button -->
-                <button class="collapse-btn">▼</button>
-                
-                <!-- Bottom options (collapsible) -->
-                <div class="moduleBottomOptions" style="display: none;">
-                    <div>
-                        <label for="sustainNotes">Sustain Notes:</label>
-                        <input type="checkbox" class="sustainNotes" checked>
-                        
-                        <label for="tonic">Tonic:</label>
-                        <select class="tonic">
-                            <option value="C">C</option>
-                            <option value="C#">C#</option>
-                            <option value="D">D</option>
-                            <option value="D#">D#</option>
-                            <option value="E">E</option>
-                            <option value="F">F</option>
-                            <option value="F#">F#</option>
-                            <option value="G">G</option>
-                            <option value="G#">G#</option>
-                            <option value="A">A</option>
-                            <option value="A#">A#</option>
-                            <option value="B">B</option>
-                        </select>
-                        
-                        <label for="scale">Scale:</label>
-                        <select class="scale">
-                            <option value="Pentatonic">Pentatonic</option>
-                            <option value="Ionian">Ionian</option>
-                            <option value="Dorian">Dorian</option>
-                            <option value="Phrygian">Phrygian</option>
-                            <option value="Lydian">Lydian</option>
-                            <option value="Mixolydian">Mixolydian</option>
-                            <option value="Aeolian">Aeolian</option>
-                            <option value="Locrian">Locrian</option>
-                            <option value="Melodic Minor">Melodic Minor</option>
-                            <option value="Harmonic Minor">Harmonic Minor</option>
-                            <option value="Harmonic Major">Harmonic Major</option>
-                            <option value="Whole Tone">Whole Tone</option>
-                            <option value="Chromatic">Chromatic</option>
-                            <option value="Octatonic Major">Octatonic Major</option>
-                            <option value="Octatonic Minor">Octatonic Minor</option>
-                            <option value="Synthetic Minor">Synthetic Minor</option>
-                            <option value="Major Bitonal">Major Bitonal</option>
-                            <option value="Minor Bitonal">Minor Bitonal</option>
-                            <option value="Polytonal">Polytonal</option>
-                            <option value="Ditone">Ditone</option>
-                            <option value="Prometheus">Prometheus</option>
-                            <option value="Trans-Pentatonic">Trans-Pentatonic</option>
-                            <option value="Pelog">Pelog</option>
-                            <option value="Slendro">Slendro</option>
-                            <option value="Hungarian-Bartok">Hungarian-Bartok</option>
-                            <option value="Bulgarian-Bartok">Bulgarian-Bartok</option>
-                            <option value="Asian-Bartok">Asian-Bartok</option>
-                            <option value="Spain">Spain</option>
-                            <option value="India-Dharmavati">India-Dharmavati</option>
-                            <option value="Japan">Japan</option>
-                            <option value="Peru">Peru</option>
-                            <option value="Alaska">Alaska</option>
-                            <option value="Jewish Ahavoh-Rabboh">Jewish Ahavoh-Rabboh</option>
-                            <option value="Slavic">Slavic</option>
-                            <option value="Blues">Blues</option>
-                        </select>
-
-                        <label for="tessitura">Tessitura:</label>
-                        <select class="tessitura">
-                            <option value="Bass">Bass</option>
-                            <option value="Baritone">Baritone</option>
-                            <option value="Tenor">Tenor</option>
-                            <option value="Alto">Alto</option>
-                            <option value="Soprano">Soprano</option>
-                        </select>
-
-                        <label for="soundTypes">Sound Type:</label>
-                        <select class="soundTypes">
-                        </select>
-                    </div>
+                <div class="readingsContainer">
+                    <label for="readings">Reading:</label>
+                    <select class="readings">
+                    </select>
                 </div>
             </div>
             
-            <!-- RIGHT SIDE: Plot -->
-            <div class="rightPlot">
-                <div class="plot"></div>
+            <div class="plot"></div>
+            
+            <button class="collapse-btn">▼</button> <!-- Collapse button -->
+            
+            <div class="moduleBottomOptions" style="display: none;"> <!-- Initially hidden -->
+                <div>
+                    <label for="sustainNotes">Sustain Notes:</label>
+                    <input type="checkbox" class="sustainNotes" checked>
+                    
+                    <label for="tonic">Tonic:</label>
+                    <select class="tonic">
+                        <option value="C">C</option>
+                        <option value="C#">C#</option>
+                        <option value="D">D</option>
+                        <option value="D#">D#</option>
+                        <option value="E">E</option>
+                        <option value="F">F</option>
+                        <option value="F#">F#</option>
+                        <option value="G">G</option>
+                        <option value="G#">G#</option>
+                        <option value="A">A</option>
+                        <option value="A#">A#</option>
+                        <option value="B">B</option>
+                    </select>
+                    
+                    <label for="scale">Scale:</label>
+                    <select class="scale">
+                        <option value="Pentatonic">Pentatonic</option>
+                        <option value="Ionian">Ionian</option>
+                        <option value="Dorian">Dorian</option>
+                        <option value="Phrygian">Phrygian</option>
+                        <option value="Lydian">Lydian</option>
+                        <option value="Mixolydian">Mixolydian</option>
+                        <option value="Aeolian">Aeolian</option>
+                        <option value="Locrian">Locrian</option>
+                        <option value="Melodic Minor">Melodic Minor</option>
+                        <option value="Harmonic Minor">Harmonic Minor</option>
+                        <option value="Harmonic Major">Harmonic Major</option>
+                        <option value="Whole Tone">Whole Tone</option>
+                        <option value="Chromatic">Chromatic</option>
+                        <option value="Octatonic Major">Octatonic Major</option>
+                        <option value="Octatonic Minor">Octatonic Minor</option>
+                        <option value="Synthetic Minor">Synthetic Minor</option>
+                        <option value="Major Bitonal">Major Bitonal</option>
+                        <option value="Minor Bitonal">Minor Bitonal</option>
+                        <option value="Polytonal">Polytonal</option>
+                        <option value="Ditone">Ditone</option>
+                        <option value="Prometheus">Prometheus</option>
+                        <option value="Trans-Pentatonic">Trans-Pentatonic</option>
+                        <option value="Pelog">Pelog</option>
+                        <option value="Slendro">Slendro</option>
+                        <option value="Hungarian-Bartok">Hungarian-Bartok</option>
+                        <option value="Bulgarian-Bartok">Bulgarian-Bartok</option>
+                        <option value="Asian-Bartok">Asian-Bartok</option>
+                        <option value="Spain">Spain</option>
+                        <option value="India-Dharmavati">India-Dharmavati</option>
+                        <option value="Japan">Japan</option>
+                        <option value="Peru">Peru</option>
+                        <option value="Alaska">Alaska</option>
+                        <option value="Jewish Ahavoh-Rabboh">Jewish Ahavoh-Rabboh</option>
+                        <option value="Slavic">Slavic</option>
+                        <option value="Blues">Blues</option>
+                    </select>
+
+                    <label for="tessitura">Tessitura:</label>
+                    <select class="tessitura">
+                        <option value="Bass">Bass</option>
+                        <option value="Baritone">Baritone</option>
+                        <option value="Tenor">Tenor</option>
+                        <option value="Alto">Alto</option>
+                        <option value="Soprano">Soprano</option>
+                    </select>
+
+                    <label for="soundTypes">Sound Type:</label>
+                    <select class="soundTypes">
+                    </select>
+                </div>
             </div>
         </div>`;
 }
