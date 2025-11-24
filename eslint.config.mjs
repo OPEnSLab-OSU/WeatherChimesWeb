081b3848200b054682331daf919c2fe86ac86b28
Rebeca Moreno Rodriguez <morenreb@oregonstate.edu>
2025-11-23 18:57:02 -0800

    Updated lint file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,5 +3,22 @@ import globals from "globals";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        // Browser-loaded libraries
+        Tone: "readonly",
+        Plotly: "readonly",
+      },
+    },
+    rules: {
+      // Ignore unused params starting with "_"
+      "no-unused-vars": ["warn", { "argsIgnorePattern": "^_" }],
+    },
+  },
 ]);
